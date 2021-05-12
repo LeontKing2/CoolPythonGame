d04a45495feb0f4dee32d7e6b13e2a0eebce8a1f
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2021-05-12 19:10:17 +0530

    Thats all i had Xd

---
+++ main.py
@@ -0,0 +1,19 @@
+import time 
+import random
+import twint 
+
+print('Hello Hi There this is just a simple game')
+time.sleep(1.5)
+print('You dont need to be here')
+time.sleep(1.5)
+print('You can press CTRL+C to Exit this game if you want to but if you want adventures Sure Why not')
+time.sleep(1.5)
+print('...')
+time.sleep(1.5)
+print('....')
+time.sleep(1.5)
+print('.....')
+time.sleep(1.5)
+print('You can ask me to add some cool stuff to this game at https://github.com/Leontking2/Leontking2')
+print('Do you wanna see all the tweets of a user?,Cuz Luckily i had The Twint Moudle Installed')
+print('If you wish to see some tweets of a user type Y if you Dont Type N')
